4790e730eabb59418d9c873a1a0406cb8b1cf3b1
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-08 10:56:18 +0800

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -40,8 +40,8 @@ repo = github.get_user(repo_owner).get_repo(repo_name)
 url = f'https://raw.githubusercontent.com/{repo_owner}/{repo_name}/master/{file_path}'
 #url=f'https://github.com/{repo_owner}/{repo_name}/blob/master/test.csv'
 response = requests.get(url)
-st.write(response.text)
-df = pd.read_csv(StringIO(response.text))
+st.write(response)
+df = pd.read_csv(response)
 df['test_col'] = "new_test_val"
 
 content = repo.get_contents(file_path)
