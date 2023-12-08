e516cf7f62f4f986ea8f44b90a6fd3baecd2c330
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-09 07:29:59 +0800

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -9,11 +9,9 @@ import pandas as pd
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit_echarts import st_echarts
-
 from streamlit.server.server import Server
 # from streamlit.script_run_context import get_script_run_ctx as get_report_ctx
 from streamlit.scriptrunner import get_script_run_ctx as get_report_ctx
-
 import graphviz
 import pydeck as pdk
 import altair as alt
@@ -23,7 +21,6 @@ from pyecharts.charts import *
 from pyecharts.globals import ThemeType
 from pyecharts import options as opts
 from pyecharts.commons.utils import JsCode
-
 from PIL import Image
 from io import BytesIO
 from github import Github
@@ -51,8 +48,9 @@ def up_datefile():
     repo.create_file("new_file.txt", "init commit", contents)
     content = repo.get_contents(file_path)
     repo.update_file(file_path, commit_message,contents, content.sha)
-up_datefile()
+
 def main():
+    up_datefile()
     st.set_page_config(page_title="七里香还是稻香",page_icon=":rainbow:",layout="wide",initial_sidebar_state="auto")
     st.title('七里香还是稻香:heart:')
     st.markdown('<br>',unsafe_allow_html=True)
