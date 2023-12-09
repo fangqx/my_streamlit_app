2a9f6f97da75580c7da62ec7c6ce5579ca270528
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-10 07:22:04 +0800

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -123,23 +123,25 @@ def main():
         card_price0=data['价格'].dropna().unique().tolist()
         card_price=['--价格: '+str(x)+' 元' for x in card_price0]
         #card_name = [a+b for a, b in zip(card_name, card_price)]
-        col1, col2,col3 = st.columns(3)       
-        col1_choice = col1.radio("单次卡", ['Option']+card_name[:4],index=0,captions=['No Selection']+card_price[:4])
-        col2_choice = col2.radio("多次卡",  ['Option']+card_name[4:9],index=0,captions=['No Selection']+card_price[4:9])
-        col3_choice = col3.radio("周月年卡",  ['Option']+card_name[9:],index=0,captions=['No Selection']+card_price[9:])
-        #card=st.radio('study',data['名称'].dropna().unique().tolist())
-        sel =  [col1_choice,col2_choice,col3_choice]
-        sel_new=[]
-        for item in sel:
-            if item!='Option':
-                sel_new.append(item)
-        st.write(sel_new)
-       
-    if len(sel_new)==1:
-        st.write('您选择的是: ', sel_new[:])
-    else:
-        
-        st.write('请重新选择')
+        with st.container():
+            st.markdown(f'### 学习计划')
+            col1, col2,col3 = st.columns(3)       
+            col1_choice = col1.radio("单次卡", ['Option']+card_name[:4],index=0,captions=['No Selection']+card_price[:4])
+            col2_choice = col2.radio("多次卡",  ['Option']+card_name[4:9],index=0,captions=['No Selection']+card_price[4:9])
+            col3_choice = col3.radio("周月年卡",  ['Option']+card_name[9:],index=0,captions=['No Selection']+card_price[9:])
+            #card=st.radio('study',data['名称'].dropna().unique().tolist())
+            sel =  [col1_choice,col2_choice,col3_choice]
+            sel_new=[]
+            for item in sel:
+                if item!='Option':
+                    sel_new.append(item)
+            st.write(sel_new)
+               
+            if len(sel_new)==1:
+                st.write('您选择的是: ', sel_new[0])
+            else:
+                
+                st.write('请重新选择')
                  
     d=st.sidebar.date_input('Date',st.session_state.date_time.date())
     t=st.sidebar.time_input('Time',st.session_state.date_time.time())
