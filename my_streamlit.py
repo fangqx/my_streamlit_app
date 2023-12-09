9538f274cba6dcc7421967611fd3ab623c965d60
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-09 19:53:59 +0800

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -122,7 +122,7 @@ def main():
     study_sel=['自习卡类型','自习时间','自习位置']
     self_study=st.sidebar.radio('自习计划选择',study_sel,index=random.choice(range(3)))
     if self_study==study_sel[0]:
-        st.write(data.columns)
+        st.write(data['名称'].dropna().unique().tolist())
         st.radio(sel_study,data['名称'].dropna().unique().tolist(),)
         
     d=st.sidebar.date_input('Date',st.session_state.date_time.date())
