3f984a8e6ae279a4756f854702110c2b2eb80053
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-10 11:13:40 +0800

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -156,8 +156,9 @@ def main():
                     col0=cols[0].date_input('开始日期',st.session_state.date_time)
                     col1=cols[1].date_input('结束日期',st.session_state.date_time,)
                     cols=st.columns(2)
-                    col2=cols[0].time_input('开始时间',step=900)
-                    col3=cols[1].time_input('结束时间')
+                    col2=cols[0].time_input('开始时间',value=None,step=3600)
+                    col3=cols[1].time_input('结束时间',value=None,step=3600)
+                    form = st.form('Manual assign')
                     submitted = st.form_submit_button("Submit")
                                         
     
