24273b35426adbf0bff66f6ebd96c376d1682995
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-12 10:14:08 +0800

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -273,9 +273,12 @@ def main():
                 #st.session_state.edited_df1 = st.session_state.new_data.copy()                
                 submitted = form.form_submit_button("修改计划",on_click=save_edits0)                 
                 if submitted:
+                    df1=st.session_state.new_data
+                    user_data_save(df1,'user_data_old.csv')
                     df1 = st.session_state.df1   
                     st.dataframe(df1)
                     user_data_save(df1,'user_data.csv')
+                    st.session_state.new_data=df1
 
                               
     
