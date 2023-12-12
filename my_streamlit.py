8f693bd3948865c26e287b2d27dac89270e2fd7d
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-12 16:25:56 +0800

    Update my_streamlit.py

--- my_streamlit.py
+++ my_streamlit.py
@@ -299,7 +299,6 @@ def main():
                         else:
                             st.session_state.percent = text_input2                      
             st.write(st.session_state.percent,st.session_state.price_sel0['价格'])
-            st.write(k)
             check1 =  any(item in sel for item in card_name[:])
             check2 =  any(item in sel0 for item in desk_num[:])
             if (check1) and (check2) and (len(st.session_state.phone)>=1) and (len(st.session_state.name)>=1):
