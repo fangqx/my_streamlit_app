3d1390e43ad31542a542b02e55f15ed406866233
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-13 19:26:27 +0800

    Update user_test.py

--- user_test.py
+++ user_test.py
@@ -138,14 +138,8 @@ def main():
         st.stop()
 
     data=up_datefile()
-    st.set_page_config(page_title="自主学习",page_icon=":rainbow:",layout="wide",initial_sidebar_state="auto")
+    #st.set_page_config(page_title="自主学习",page_icon=":rainbow:",layout="wide",initial_sidebar_state="auto")
     st.title('自主学习--提高效率:heart:')
-    st.markdown('<br>',unsafe_allow_html=True)
-    st.markdown('<br>',unsafe_allow_html=True)
-    charts_mapping={
-        'Line':'line_chart','Bar':'bar_chart','Area':'area_chart','Hist':'pyplot','Altair':'altair_chart',
-        'Map':'map','Distplot':'plotly_chart','Pdk':'pydeck_chart','Graphviz':'graphviz_chart','PyEchart':''
-    }
     if 'first_visit' not in st.session_state:
         st.session_state.first_visit=True
     else:
