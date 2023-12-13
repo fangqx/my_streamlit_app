920be5c1270a350b4df51e6aa5501ad7939f98a8
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-13 18:51:36 +0800

    Update user_test.py

--- user_test.py
+++ user_test.py
@@ -20,10 +20,13 @@ import hmac
 
 def check_password():
     """Returns `True` if the user had the correct password."""
-    password0="159532"
+    if pass not in st.session_state:
+        st.session_state.pass="159532"
+    else:
+        st.session_state.pass="159532"
     def password_entered(password0):
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], password0):
+        if hmac.compare_digest(st.session_state["password"], st.session_state.pass):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the password.
         else:
