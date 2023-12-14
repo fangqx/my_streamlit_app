a1cbb943701ffc9ef154674404a33187bc30256c
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-14 20:20:30 +0800

    Update user_test.py

--- user_test.py
+++ user_test.py
@@ -165,6 +165,7 @@ def main():
             card_price0=data['价格'].tolist()
             card_price=['--价格: '+str(x)+' 元' for x in card_price0]
             card_time=data[data['学习卡']==st.session_state.card_type]
+            st.write(data['学习卡'],st.session_state.card_type)
             #card_name = [a+b for a, b in zip(card_name, card_price)]
             with st.expander("时间阶段选择",expanded=True):
                 #st.write(sel_new,card_name[:])
