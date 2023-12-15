2f2e53b23e1a94d5d68fb75d2346c5b9c4a4ebfd
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-15 21:09:40 +0800

    Update user_test.py

--- user_test.py
+++ user_test.py
@@ -321,7 +321,8 @@ def main():
             
                 if submit_button:
                     user_data_write(df_new,user_file) 
-         with st.expander("查看学习计划",expanded=True):      
+                    
+         with st.expander("查看学习计划",expanded=False):      
              new_user_data=user_data_read(user_file)
              your_all_data=user_data[user_data['手机号'].astype(str)==st.session_state.phone_num]
              st.dataframe(your_all_data)
