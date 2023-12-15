f873e468e682164560a4a54009e848ee23cab3be
Quanxiao Fang <31942795+fangqx@users.noreply.github.com>
2023-12-15 14:56:06 +0800

    Update user_test.py

--- user_test.py
+++ user_test.py
@@ -166,7 +166,7 @@ def main():
     if not check_password(use_data):
         st.stop()
     st.title('自主学习--提高效率:heart:')
-    st.write(st.session_state.pass)
+    st.write(st.session_state.pass0)
     use_data=user_data_read('user_data.csv')
     st.write(use_data['手机号'].astype(str).to_list())
     your_data=use_data[use_data['手机号'].astype(str)==st.session_state.pass]
